afb055c0662c817e328386a50eac266f30338b56
Rafael Gumieri <rgumieri@gmail.com>
2017-06-12 10:02:03 -0300

    experimenting andlabs ui

--- cmd/setup/main.go
+++ cmd/setup/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/andlabs/ui"
 	"github.com/gumieri/huexe/lib/api"
 	"io/ioutil"
-	"log"
 	"net/http"
 )
 
@@ -13,33 +13,71 @@ type NUPnP struct {
 	Address string `json:"internalipaddress"`
 }
 
-func main() {
-	var err error
+func setup() (err error) {
+	config := new(api.Config)
+
+	// Defaults
+	config.Id = 1
+	config.Address = ""
+	config.Steps = 25
 
 	url := "https://www.meethue.com/api/nupnp"
 	response, err := http.Get(url)
 
 	if err != nil {
-		log.Fatal(err)
 		return
 	}
 
 	defer response.Body.Close()
 
 	responseBytes, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		return
+	}
+
 	keys := make([]NUPnP, 0)
 	json.Unmarshal(responseBytes, &keys)
 
-	config := new(api.Config)
-
-	// Defaults
-	config.Id = 1
-	config.Address = keys[0].Address
-	config.Steps = 25
+	if len(keys) > 0 {
+		config.Address = keys[0].Address
+	}
 
 	err = api.SetConfig(config)
 
 	if err != nil {
-		log.Fatal(err)
+		return
+	}
+
+	return
+}
+
+func mainUi() {
+	button := ui.NewButton("Setup")
+
+	box := ui.NewVerticalBox()
+	box.Append(button, false)
+
+	window := ui.NewWindow("Setup", 200, 100, false)
+	window.SetChild(box)
+
+	button.OnClicked(func(*ui.Button) {
+		setup()
+		return
+	})
+
+	window.OnClosing(func(*ui.Window) bool {
+		ui.Quit()
+		return true
+	})
+
+	window.Show()
+}
+
+func main() {
+	err := ui.Main(mainUi)
+
+	if err != nil {
+		panic(err)
 	}
 }
